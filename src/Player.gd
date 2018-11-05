3ed6cf67f362332dd9a71c26152b3c34c19bd327
MarkCangila <keonsanavandijoon@gmail.com>
2018-11-05 16:49:46 -0500

    Added test level and started main menu

---
+++ src/Player.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+export(int) var gravity
+export(int) var walkspeed
+export(int) var jumpspeed
+var velocity = Vector2()
+var xscl
+var yscl
+
+func _ready():
+	xscl = (get_viewport().size.x / 1024) * .5
+	yscl = (get_viewport().size.y / 600) * .5
+	# Scale the sprites
+	$TopSprite.scale = Vector2(xscl, yscl)
+	$TopSprite.position = Vector2(0, -((xscl * 100) / 2))
+	$BottomSprite.scale = Vector2(xscl, yscl)
+	$BottomSprite.position = Vector2(0, (xscl * 100) / 2)
+	# Scale the collider
+	var transform = $Hitbox.shape
+	var oldscale = transform.extents
+	transform.extents = Vector2(oldscale.x * (xscl * 2), oldscale.y * (2 * yscl))
+	
+func _physics_process(delta):
+	velocity.y += delta * gravity
+	velocity.x = 0
+	if (Input.is_action_pressed("ui_right")):
+		velocity.x += walkspeed
+	if (Input.is_action_pressed("ui_left")):
+		velocity.x -= walkspeed
+	if (Input.is_action_just_pressed("ui_space") && is_on_floor()):
+		velocity.y = -jumpspeed
+		
+	move_and_slide(velocity, Vector2(0, -1))
