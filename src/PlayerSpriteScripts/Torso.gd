2bc595b240c9429e15402e7f280fac89e0f6a35d
VeryCleverName3 <07jamesemr@csdecatur.net>
2018-11-11 08:59:26 -0500

    Hopefully this works?

---
+++ src/PlayerSpriteScripts/Torso.gd
@@ -0,0 +1,20 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var baseSprite
+var player
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	baseSprite = get_node(")
+	
+	
+
+func _process(delta):
+	pass
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+
